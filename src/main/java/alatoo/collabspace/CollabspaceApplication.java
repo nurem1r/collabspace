e3929e63fd1898357f829e0dcd0eda7e5a3225cf
nurem1r <nuremir.sulaimanov@alatoo.edu.kg>
2025-11-13 11:57:34 +0600

    first commit

---
+++ src/main/java/alatoo/collabspace/CollabspaceApplication.java
@@ -0,0 +1,13 @@
+package alatoo.collabspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabspaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alatoo/collabspace/CollabspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package alatoo.collabspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
