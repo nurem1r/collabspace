23b33f754e1728829c8e850a7736cde9ff71e701
nurem1r <nuremir.sulaimanov@alatoo.edu.kg>
2025-12-17 18:09:09 +0600

    fix: created new entities to handle an application request to projects

---
+++ src/main/java/alatoo/collabspace/entities/ProjectApplication.java
@@ -0,0 +1,42 @@
+package alatoo.collabspace.entities;
+
+import alatoo.collabspace.entities.enums.ApplicationStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "project_applications")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProjectApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(columnDefinition = "text")
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationStatus status;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        if (createdAt == null) createdAt = LocalDateTime.now();
+        if (status == null) status = ApplicationStatus.PENDING;
+    }
+}
