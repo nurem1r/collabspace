670924cd2e900f8081dd95b4a9eb8c4709c81876
Aisuluu Sharipova <@asharipova>
2025-12-18 12:28:26 +0600

    feat: repo for skill implemented

---
+++ src/main/java/alatoo/collabspace/repositories/SkillRepository.java
@@ -0,0 +1,10 @@
+package alatoo.collabspace.repositories;
+
+import alatoo.collabspace.entities.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+    Optional<Skill> findByName(String name);
+}
