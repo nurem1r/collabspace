7c3a7c1a3771dbbef4da2164586ced24af050df6
AisuluuSharipova <zhoodarbekovaaisuluu@gmail.com>
2025-12-18 17:19:35 +0600

    feat: basic config of BCrypt is initialized

---
+++ src/main/java/alatoo/collabspace/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package alatoo.collabspace.config;
+
+import alatoo.collabspace.services.impl.UserServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
