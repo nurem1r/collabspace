134c5ef316151f8122a9277ba45cbd723788a4ff
nurem1r <nuremir.sulaimanov@alatoo.edu.kg>
2025-12-18 19:04:26 +0600

    feat: improve controllers to use new endpoints in UserAchievementController

--- src/main/java/alatoo/collabspace/controllers/UserAchievementController.java
+++ src/main/java/alatoo/collabspace/controllers/UserAchievementController.java
@@ -3,10 +3,12 @@ package alatoo.collabspace.controllers;
 import alatoo.collabspace.dto.UserAchievementDto;
 import alatoo.collabspace.services.UserAchievementService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web. bind.annotation.*;
 
-import java.util.List;
+import java. util.List;
 
 @RestController
 @RequestMapping("/api/user-achievements")
@@ -27,6 +29,16 @@ public class UserAchievementController {
     @GetMapping
     public ResponseEntity<List<UserAchievementDto>> list() {
         return ResponseEntity.ok(service.listAll());
+    }
+
+    @GetMapping("/paged")
+    public ResponseEntity<Page<UserAchievementDto>> listPaged(Pageable pageable) {
+        return ResponseEntity.ok(service.listAllPaged(pageable));
+    }
+
+    @PostMapping("/{id}/verify")
+    public ResponseEntity<UserAchievementDto> verify(@PathVariable Long id, @RequestParam Long verifierId) {
+        return ResponseEntity.ok(service. verify(id, verifierId));
     }
 
     @DeleteMapping("/{id}")
