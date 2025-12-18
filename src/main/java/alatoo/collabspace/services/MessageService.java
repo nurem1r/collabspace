a1805c18400d6f372c2e7dc87482f06efecf0ef3
Karimova1410 <roza.karimova@alatoo.edu.kg>
2025-12-18 18:39:59 +0600

    feat: create new service interface MessageService to handle more endpoints

--- src/main/java/alatoo/collabspace/services/MessageService.java
+++ src/main/java/alatoo/collabspace/services/MessageService.java
@@ -1,6 +1,8 @@
 package alatoo.collabspace.services;
 
-import alatoo.collabspace.dto.MessageDto;
+import alatoo. collabspace.dto.MessageDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -8,5 +10,6 @@ public interface MessageService {
     MessageDto create(MessageDto dto);
     MessageDto getById(Long id);
     List<MessageDto> listByChat(Long chatId);
+    Page<MessageDto> listByChatPaged(Long chatId, Pageable pageable);
     void delete(Long id);
 }
