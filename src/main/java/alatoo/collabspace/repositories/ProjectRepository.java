df0248ea8086968f3fbd0338745464116a5e461c
Aisuluu Sharipova <@asharipova>
2025-12-18 12:28:59 +0600

    feat: repo for project implemented

---
+++ src/main/java/alatoo/collabspace/repositories/ProjectRepository.java
@@ -0,0 +1,7 @@
+package alatoo.collabspace.repositories;
+
+import alatoo.collabspace.entities.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
