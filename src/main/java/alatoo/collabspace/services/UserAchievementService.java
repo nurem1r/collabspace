1a744d4d238dfe05de1513084f05e768762148a1
Karimova1410 <roza.karimova@alatoo.edu.kg>
2025-12-18 18:35:52 +0600

    feat: create new service interface to handle UserAchievementService more endpoints

--- src/main/java/alatoo/collabspace/services/UserAchievementService.java
+++ src/main/java/alatoo/collabspace/services/UserAchievementService.java
@@ -1,6 +1,8 @@
 package alatoo.collabspace.services;
 
 import alatoo.collabspace.dto.UserAchievementDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -8,5 +10,7 @@ public interface UserAchievementService {
     UserAchievementDto assign(UserAchievementDto dto);
     UserAchievementDto getById(Long id);
     List<UserAchievementDto> listAll();
+    Page<UserAchievementDto> listAllPaged(Pageable pageable);
+    UserAchievementDto verify(Long id, Long verifierId);
     void delete(Long id);
 }
