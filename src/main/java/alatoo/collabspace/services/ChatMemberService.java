116878bda55d5924165a8ce247c12b62b4397dab
Karimova1410 <roza.karimova@alatoo.edu.kg>
2025-12-18 18:38:58 +0600

    feat: create new service interface ChatMemberService to handle more endpoints

--- src/main/java/alatoo/collabspace/services/ChatMemberService.java
+++ src/main/java/alatoo/collabspace/services/ChatMemberService.java
@@ -1,6 +1,8 @@
 package alatoo.collabspace.services;
 
 import alatoo.collabspace.dto.ChatMemberDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -8,5 +10,6 @@ public interface ChatMemberService {
     ChatMemberDto add(ChatMemberDto dto);
     ChatMemberDto getById(Long id);
     List<ChatMemberDto> listByChat(Long chatId);
+    Page<ChatMemberDto> listAllPaged(Pageable pageable);
     void remove(Long id);
 }
