77a7815be36515531d0929da39350a63ae5c69e1
Karimova1410 <roza.karimova@alatoo.edu.kg>
2025-12-18 18:40:55 +0600

    feat: create new service interface ProjectRequiredRoleService to handle more endpoints

--- src/main/java/alatoo/collabspace/services/ProjectRequiredRoleService.java
+++ src/main/java/alatoo/collabspace/services/ProjectRequiredRoleService.java
@@ -1,6 +1,8 @@
 package alatoo.collabspace.services;
 
-import alatoo.collabspace.dto.ProjectRequiredRoleDto;
+import alatoo.collabspace. dto.ProjectRequiredRoleDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain. Pageable;
 
 import java.util.List;
 
@@ -8,6 +10,7 @@ public interface ProjectRequiredRoleService {
     ProjectRequiredRoleDto create(ProjectRequiredRoleDto dto);
     ProjectRequiredRoleDto getById(Long id);
     List<ProjectRequiredRoleDto> listByProject(Long projectId);
+    Page<ProjectRequiredRoleDto> listAllPaged(Pageable pageable);
     ProjectRequiredRoleDto update(Long id, ProjectRequiredRoleDto dto);
     void delete(Long id);
 }
