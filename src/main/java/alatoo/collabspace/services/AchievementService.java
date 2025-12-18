461989140ff690511a8a3e58f15a90861c96d039
Karimova1410 <roza.karimova@alatoo.edu.kg>
2025-12-18 18:39:27 +0600

    feat: create new service interface AchievementService to handle more endpoints

--- src/main/java/alatoo/collabspace/services/AchievementService.java
+++ src/main/java/alatoo/collabspace/services/AchievementService.java
@@ -1,6 +1,9 @@
 package alatoo.collabspace.services;
 
 import alatoo.collabspace.dto.AchievementDto;
+import alatoo.collabspace.entities.enums.AchievementSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -8,6 +11,10 @@ public interface AchievementService {
     AchievementDto create(AchievementDto dto);
     AchievementDto getById(Long id);
     List<AchievementDto> listAll();
+    Page<AchievementDto> listAllPaged(Pageable pageable);
     AchievementDto update(Long id, AchievementDto dto);
     void delete(Long id);
+    
+    // Поиск
+    List<AchievementDto> searchAchievements(AchievementSource source, Long skillId);
 }
