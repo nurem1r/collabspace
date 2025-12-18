246d81ff58e7460630d3370bef234685dbfd8b60
Karimova1410 <roza.karimova@alatoo.edu.kg>
2025-12-18 18:40:37 +0600

    feat: create new service interface ProjectMemberService to handle more endpoints

--- src/main/java/alatoo/collabspace/services/ProjectMemberService.java
+++ src/main/java/alatoo/collabspace/services/ProjectMemberService.java
@@ -1,6 +1,8 @@
 package alatoo.collabspace.services;
 
 import alatoo.collabspace.dto.ProjectMemberDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -8,6 +10,7 @@ public interface ProjectMemberService {
     ProjectMemberDto add(ProjectMemberDto dto);
     ProjectMemberDto getById(Long id);
     List<ProjectMemberDto> listByProject(Long projectId);
+    Page<ProjectMemberDto> listAllPaged(Pageable pageable);
     ProjectMemberDto rateMember(Long id, Integer rating, String feedback);
     void remove(Long id);
 }
