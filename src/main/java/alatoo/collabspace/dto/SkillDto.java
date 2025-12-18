73de3fad25409b50a3b4a8dd48d1af0180af67ec
Syrgak13 <syrgaktabyldiev13@gmail.com>
2025-12-18 16:19:20 +0600

    Added SkillDto

---
+++ src/main/java/alatoo/collabspace/dto/SkillDto.java
@@ -0,0 +1,13 @@
+package alatoo.collabspace.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SkillDto {
+    private Long id;
+    private String name;
+}
