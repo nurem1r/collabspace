7907d756b74193aae8b1ca7d00002aa54939e2bf
Karimova1410 <roza.karimova@alatoo.edu.kg>
2025-12-18 18:39:45 +0600

    feat: create new service interface ChatService to handle more endpoints

--- src/main/java/alatoo/collabspace/services/ChatService.java
+++ src/main/java/alatoo/collabspace/services/ChatService.java
@@ -1,12 +1,16 @@
 package alatoo.collabspace.services;
 
 import alatoo.collabspace.dto.ChatDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface ChatService {
     ChatDto create(ChatDto dto);
+    ChatDto createPrivateChat(Long user1Id, Long user2Id);
     ChatDto getById(Long id);
     List<ChatDto> listAll();
+    Page<ChatDto> listAllPaged(Pageable pageable);
     void delete(Long id);
 }
